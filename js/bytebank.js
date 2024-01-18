15eedd407dd8d0581158c767d2048a1a6f580854
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-18 16:48:36 -0300

    Update bytebank.js
    form

---
+++ js/bytebank.js
@@ -0,0 +1,12 @@
+let saldo = 3000;
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+elementoSaldo.textContent = saldo;
+
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (!elementoFormulario.checkValidity()) {
+    alert("Please, fill all the fields!");
+    return;
+  }
+});
