22a486abb01008331218c1d4577d44a198630415
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-22 18:36:51 -0300

    setting
    js to ts and fixiing bugs

--- js/bytebank.js
+++ js/bytebank.js
@@ -1,40 +1,37 @@
-let saldo = 3000;
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = saldo;
-
-const elementoFormulario = document.querySelector(".block-nova-transacao form");
+var saldo = 3000;
+var elementoSaldo = document.querySelector(".saldo-valor .valor");
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+var elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (!elementoFormulario.checkValidity()) {
-    alert("Please, fill all the fields!");
-    return;
-  }
-
-  const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
-  const inputValor = elementoFormulario.querySelector("#valor");
-  const inputData = elementoFormulario.querySelector("#data");
-
-  let tipoTransacao = inputTipoTransacao.value;
-  let valor = inputValor.value;
-  let data = inputData.value;
-
-  if (tipoTransacao == "Deposit") {
-    saldo += Number(valor);
-  } else if (tipoTransacao == "Transfer" || tipoTransacao == "Bill Payment") {
-    saldo -= valor;
-  } else {
-    alert("Transfer type invalid");
-    return;
-  }
-
-  elementoSaldo.textContent = saldo;
-
-  const novaTransacaoo = {
-    tipoTransacao: tipoTransacao,
-    valor: valor,
-    data: data,
-  };
-
-  console.log(novaTransacaoo);
-  elementoFormulario.reset();
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Please, fill all the fields!");
+        return;
+    }
+    var inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    var inputValor = elementoFormulario.querySelector("#valor");
+    var inputData = elementoFormulario.querySelector("#data");
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = inputValor.valueAsNumber;
+    var data = new Date(inputData.value);
+    if (tipoTransacao == "Deposit") {
+        saldo += valor;
+    }
+    else if (tipoTransacao == "Transfer" || tipoTransacao == "Bill Payment") {
+        saldo -= valor;
+    }
+    else {
+        alert("Transfer type invalid");
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString();
+    var novaTransacaoo = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data,
+    };
+    console.log(novaTransacaoo);
+    elementoFormulario.reset();
 });
---
+++ js/bytebank.ts
@@ -0,0 +1,52 @@
+let saldo = 3000;
+const elementoSaldo = document.querySelector(
+  ".saldo-valor .valor"
+) as HTMLElement;
+if (elementoSaldo != null) {
+  elementoSaldo.textContent = saldo.toString();
+}
+
+const elementoFormulario = document.querySelector(
+  ".block-nova-transacao form"
+) as HTMLFormElement;
+elementoFormulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (!elementoFormulario.checkValidity()) {
+    alert("Please, fill all the fields!");
+    return;
+  }
+
+  const inputTipoTransacao = elementoFormulario.querySelector(
+    "#tipoTransacao"
+  ) as HTMLSelectElement;
+  const inputValor = elementoFormulario.querySelector(
+    "#valor"
+  ) as HTMLInputElement;
+  const inputData = elementoFormulario.querySelector(
+    "#data"
+  ) as HTMLInputElement;
+
+  let tipoTransacao: string = inputTipoTransacao.value;
+  let valor: number = inputValor.valueAsNumber;
+  let data: Date = new Date(inputData.value);
+
+  if (tipoTransacao == "Deposit") {
+    saldo += valor;
+  } else if (tipoTransacao == "Transfer" || tipoTransacao == "Bill Payment") {
+    saldo -= valor;
+  } else {
+    alert("Transfer type invalid");
+    return;
+  }
+
+  elementoSaldo.textContent = saldo.toString();
+
+  const novaTransacaoo = {
+    tipoTransacao: tipoTransacao,
+    valor: valor,
+    data: data,
+  };
+
+  console.log(novaTransacaoo);
+  elementoFormulario.reset();
+});
