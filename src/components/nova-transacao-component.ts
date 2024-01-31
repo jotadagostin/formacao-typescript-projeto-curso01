c245fcc86c50a6f89189ab4391ade5517d9693b8
João Paulo Dagostin <dagostinjota@gmail.com>
2024-01-31 10:01:54 -0300

    ts code
    using localStorage

--- src/components/nova-transacao-component.ts
+++ src/components/nova-transacao-component.ts
@@ -6,34 +6,40 @@ import SaldoComponent from "./saldo-component.js";
 const elementoFormulario = document.querySelector(
   ".block-nova-transacao form"
 ) as HTMLFormElement;
+
 elementoFormulario.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (!elementoFormulario.checkValidity()) {
-    alert("Please, fill all the fields!");
-    return;
+  try {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+      alert("Please, fill all the fields!");
+      return;
+    }
+
+    const inputTipoTransacao = elementoFormulario.querySelector(
+      "#tipoTransacao"
+    ) as HTMLSelectElement;
+    const inputValor = elementoFormulario.querySelector(
+      "#valor"
+    ) as HTMLInputElement;
+    const inputData = elementoFormulario.querySelector(
+      "#data"
+    ) as HTMLInputElement;
+
+    let tipoTransacao: TipoTransacao =
+      inputTipoTransacao.value as TipoTransacao;
+    let valor: number = inputValor.valueAsNumber;
+    let data: Date = new Date(inputData.value);
+
+    const novaTransacaoo: Transacao = {
+      tipoTransacao: tipoTransacao,
+      valor: valor,
+      data: data,
+    };
+
+    Conta.registrarTransacao(novaTransacaoo);
+    SaldoComponent.atualizar();
+    elementoFormulario.reset();
+  } catch (error) {
+    alert(error.message);
   }
-
-  const inputTipoTransacao = elementoFormulario.querySelector(
-    "#tipoTransacao"
-  ) as HTMLSelectElement;
-  const inputValor = elementoFormulario.querySelector(
-    "#valor"
-  ) as HTMLInputElement;
-  const inputData = elementoFormulario.querySelector(
-    "#data"
-  ) as HTMLInputElement;
-
-  let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
-  let valor: number = inputValor.valueAsNumber;
-  let data: Date = new Date(inputData.value);
-
-  const novaTransacaoo: Transacao = {
-    tipoTransacao: tipoTransacao,
-    valor: valor,
-    data: data,
-  };
-
-  Conta.registrarTransacao(novaTransacaoo);
-  SaldoComponent.atualizar();
-  elementoFormulario.reset();
 });
--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,6 +1,33 @@
 import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
+
 let saldo: number = 3000;
+
+const transacoes: Transacao[] =
+  JSON.parse(
+    localStorage.getItem("transacoes"),
+    (key: string, value: string) => {
+      if (key === "data") {
+        return new Date(value);
+      }
+    }
+  ) || [];
+
+function debitar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error("the value to be debit must be bigger than zero");
+  }
+  if (valor > saldo) {
+    throw new Error("Value not avalueble");
+  }
+}
+
+function depositar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error("the value to be debit must be bigger than zero");
+  }
+  saldo += valor;
+}
 
 const Conta = {
   getSaldo() {
@@ -13,16 +40,19 @@ const Conta = {
 
   registrarTransacao(novaTransacaoo: Transacao): void {
     if (novaTransacaoo.tipoTransacao == TipoTransacao.DEPOSIT) {
-      saldo += novaTransacaoo.valor;
+      depositar(novaTransacaoo.valor);
     } else if (
       novaTransacaoo.tipoTransacao == TipoTransacao.TRANSFER ||
       novaTransacaoo.tipoTransacao == TipoTransacao.PAYMENT_BILL
     ) {
-      saldo -= novaTransacaoo.valor;
+      debitar(novaTransacaoo.valor);
     } else {
-      alert("Transfer type invalid");
-      return;
+      throw new Error("Transfer type invalid");
     }
+
+    transacoes.push(novaTransacaoo);
+    console.log(transacoes);
+    localStorage.setItem("transacoes", JSON.stringify(transacoes));
   },
 };
 export default Conta;
